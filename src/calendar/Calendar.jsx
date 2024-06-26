380b8e49cc463a6dd6456aa656658cb49d0108f4
eavMarshall <elliottavmarshall@gmail.com>
2024-06-26 19:23:08 +1200

    building out the table

--- src/calendar/Calendar.jsx
+++ src/calendar/Calendar.jsx
@@ -1,17 +1,45 @@
 import styles from './Calendar.module.scss';
+import './Calendar.scss';
 import {CalendarStore} from "./store/Calendar.store";
+
+const time = {
+    0: [15, 30, 45],
+    60: [15, 30, 45],
+    120: [15, 30, 45],
+    180: [15, 30, 45],
+    240: [15, 30, 45],
+    300: [15, 30, 45],
+    360: [15, 30, 45],
+    420: [15, 30, 45],
+};
 
 function Calendar() {
     const allStaff = CalendarStore((state) => state.allStaff);
     return (
-        <div className="Calendar">
-            <div className={styles.header}>
-            {Object.keys(allStaff).map((id) => {
-                const staff = allStaff[id];
-                return <div key={`Staff-${id}`}>{staff.name}</div>;
-            })}
+        <div className={styles.pageWrapper}>
+            <div className="calendar table-container">
+                <table border="1">
+                    <tr>
+                        <th rowSpan="1"></th>
+                        {Object.keys(allStaff).map((id) => {
+                            const staff = allStaff[id];
+                            return <th key={`Staff-${id}`} className={styles.header}>{staff.name}</th>;
+                        })}
+                    </tr>
+                    {Object.keys(time).map((id, i) => {
+                        return time[id].map((timeRow, j) => {
+                            return <>
+                                <tr>
+                                    {j === 0 ? <td rowSpan="3" className={styles.timeColumn}></td> : null}
+                                    {Object.keys(allStaff).map((id) => {
+                                        return <td key={`Staff-${id}`} className={styles.timeHeight}></td>;
+                                    })}
+                                </tr>
+                            </>
+                        });
+                    })}
+                </table>
             </div>
-
         </div>
     );
 }
